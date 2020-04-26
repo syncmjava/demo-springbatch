809f81ae151a0789f9523a317594b7fedcf3e8d4
changming.sun <changming.sun@accenture.com>
2020-04-26 18:01:08 +0800

    init commit

---
+++ src/main/java/com/demo/DemoSpringbatchApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbatchApplication.class, args);
+	}
+
+}
